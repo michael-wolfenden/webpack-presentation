16602a89f7527e4f5cb6e571d44a18ea9208a33e
unknown <mwolfend@D45107TF.sunqld.com.au>
2015-06-23 10:29:17 +1000

    script tags are overrated

--- configuration.js
+++ configuration.js
@@ -5,11 +5,13 @@ var srcDir = path.join(__dirname, 'src');
 var buildDir = path.join(__dirname, 'build');
 var appDir = path.join(srcDir, 'app');
 var entryFile = path.join(appDir, 'app.module.js');
+var index = path.join(srcDir, 'index.html');
 
 var exports = {
     paths: {
         entryFile: entryFile,
-        buildDir: buildDir
+        buildDir: buildDir,
+        index: index
     }
 };
 
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -2,13 +2,21 @@
 
 var config = require('./configuration');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var webpack = {
     entry: config.paths.entryFile,
     output: {
         filename: 'bundle.js',
         path: config.paths.buildDir
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: config.paths.index,
+            inject: true
+        })
+    ]
 };
 
 module.exports = webpack;
